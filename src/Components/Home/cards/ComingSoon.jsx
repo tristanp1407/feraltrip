163ee1d1068336b18de301a305493146d4b5c76a
tristanp1407 <tristan.plet@gmail.com>
2021-05-17 17:52:49 +0100

    spell check

--- src/Components/Home/cards/ComingSoon.jsx
+++ src/Components/Home/cards/ComingSoon.jsx
@@ -16,7 +16,7 @@ class ComingSoon extends Component {
             alt=""
             onClick={this.props.toggle}
           />
-          <h2>Comming Soon!</h2>
+          <h2>Coming Soon!</h2>
           <br />
           <br />
           <p>Chekout our trip in the Peak District</p>
